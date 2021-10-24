4fcee0148403e811a3dcd13dbb816f82dd194e86
robemadz <robemadz@gmail.com>
2021-10-24 23:09:00 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { ImageCard } from "./components/ImageCard/ImageCard";
+
+function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="container mx-auto">
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">
+          Cargando imágenes
+        </h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
