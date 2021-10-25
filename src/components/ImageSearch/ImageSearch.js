adcd4dc3b43e41ced4defc1a7b59b4882c03bba3
robemadz <robemadz@gmail.com>
2021-10-25 19:06:22 +0200

    funcionalidades terminadas

--- src/components/ImageSearch/ImageSearch.js
+++ src/components/ImageSearch/ImageSearch.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ImageSearch = () => {
-  return <div></div>;
+const ImageSearch = ({ searchText }) => {
+  const [text, setText] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    searchText(text);
+  };
+
+  return (
+    <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
+      <form onSubmit={onSubmit} className="w-full max-w-sm">
+        <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
+          <input
+            type="text"
+            onChange={(e) => setText(e.target.value)}
+            className="appearence-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            placeholder="Buscar imagen"
+          />
+          <button
+            className="flex-shrink-0 bg-indigo-700 hover:bg-indigo-800 border-indigo-700 hover:border-indigo-700 text-sm border-4 text-white py-1 px-2 rounded"
+            type="submit"
+          >
+            Buscar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ImageSearch;
